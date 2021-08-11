ca1ace017294edf5b01406b9aa33cbe54f0da60e
anhddph10755 <anhddph10755@fpt.edu.vn>
2021-08-11 13:08:21 +0700

    update 11/8

--- frontend/src/pages/ProductDetail.js
+++ frontend/src/pages/ProductDetail.js
@@ -118,7 +118,7 @@ const ProductDetail = {
                         </div>-->
                         <div class="flex mt-6">
                             <span class="title-font font-medium text-2xl text-gray-900">${product.price} VNĐ</span>
-                            <button class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Mua ngay</button>
+                            <button id="add-to-cart" class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Mua ngay</button>
                             <button class="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
                                 <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
                                 <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
